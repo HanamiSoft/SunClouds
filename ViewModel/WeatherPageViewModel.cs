331fa06fc1780cc58db7f64ad025576d9b7755f8
HanamiLux <oxotnikzn2017@yandex.ru>
2023-06-15 00:53:59 +0300

    Созданы все окна и страницы в MVVM

---
+++ ViewModel/WeatherPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SunClouds.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunClouds.ViewModel
+{
+    internal class WeatherPageViewModel : BindingHelper
+    {
+    }
+}
