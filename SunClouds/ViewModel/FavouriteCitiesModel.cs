27058d715b68a07542d06c8d3f60912cc6731e53
fisuyyi <fisuyyi@gmail.com>
2023-06-24 22:53:41 +0300

    Сделал выгрузку UC в WrapPanel, стили RadioButton сделаю позже, это функционалу не помешает.

---
+++ SunClouds/ViewModel/FavouriteCitiesModel.cs
@@ -0,0 +1,34 @@
+﻿using SunClouds.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunClouds.ViewModel
+{
+    class FavouriteCitiesModel : BindingHelper
+    {
+        private string country;
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+                OnPropertyChanged(nameof(Country));
+            }
+        }
+
+        private string shirota;
+        public string Shirota
+        {
+            get { return shirota; }
+            set
+            {
+                shirota = value;
+                OnPropertyChanged(nameof(Shirota));
+            }
+        }
+    }
+}
--- SunClouds/ViewModel/SettingsViewModel.cs
+++ SunClouds/ViewModel/SettingsViewModel.cs
@@ -1,14 +1,24 @@
 ﻿using SunClouds.ViewModel.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SunClouds.ViewModel
 {
-    internal class SettingsViewModel : BindingHelper
-    {
 
+    class SettingsViewModel : BindingHelper
+    {
+        public ObservableCollection<FavouriteCitiesModel> Items { get; } = new ObservableCollection<FavouriteCitiesModel>();
+
+        public SettingsViewModel()
+        {
+            // Добавьте элементы в коллекцию
+            Items.Add(new FavouriteCitiesModel { Country = "Item 1", Shirota = "Shirota 1" });
+            Items.Add(new FavouriteCitiesModel { Country = "Item 2", Shirota = "Shirota 2" });
+            Items.Add(new FavouriteCitiesModel { Country = "Item 3", Shirota = "Shirota 3" });
+        }
     }
 }
